837116c395e1814071162830a208a75c6ef0356f
Janusz Chudzynski <janusz@izotx.com>
2018-12-02 16:44:20 -0600

    Added NSNotification

--- MacCalculatorObjectiveC/MacCalculatorObjectiveC/ViewController.m
+++ MacCalculatorObjectiveC/MacCalculatorObjectiveC/ViewController.m
@@ -10,31 +10,86 @@
 #import "OperationResult.h"
 #import "SimpleCalculator.h"
 
-@interface ViewController()
-    @property SimpleCalculator* calculator;
+@interface ViewController() <NSTextFieldDelegate,NSControlTextEditingDelegate>
+@property (weak) IBOutlet NSTextField *labelField;
+@property (weak) IBOutlet NSTextField *displayField;
+@property SimpleCalculator* calculator;
 @end
 
 @implementation ViewController
 
 - (IBAction)buttonAction:(id)sender {
-    NSLog(@" %@", [(NSButton *)sender title] );
-   [_calculator addExpression:[sender title]];
-    OperationResult * result =  _calculator.currentResult;
-    NSLog(@" %d", result.value);
+    [self addExpression:[sender title]];
+    [self refreshScreen];
 }
 
+
+-(void)addExpression:(NSString*)expression{
+   [_calculator addExpression:expression];
+}
+
+-(void)refreshScreen{
+    OperationResult * result =  _calculator.currentResult;
+    [_displayField setStringValue:[NSString stringWithFormat:@"%d",result.value]];
+    [_labelField setStringValue:[NSString stringWithFormat:@"%@",result.message]];
+
+
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     _calculator = [SimpleCalculator new];
+    _displayField.delegate = self;
+    _labelField.editable = NO;
+    //Add Observer
+    [_calculator addObserver:self forKeyPath:@"currentResult" options:NSKeyValueObservingOptionNew context:nil];
+    
+    
     // Do any additional setup after loading the view.
 }
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context{
+    NSLog(@"Observe Value: %@ %@",keyPath,[change objectForKey:@"new"]);
+    [self refreshScreen];
+    
+}
+
 
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
 
     // Update the view, if already loaded.
+}
+
+
+- (BOOL)control:(NSControl *)control textShouldEndEditing:(NSText *)fieldEditor{
+    
+    NSLog(@"SHould End Editing %@",fieldEditor);
+    return true;
+}
+
+-(void)controlTextDidChange:(NSNotification *)notification{
+    NSString * val = [(NSTextField*) notification.object stringValue];
+    NSString*  last = [val substringWithRange:NSMakeRange(val.length -1, 1)];
+    NSLog(@"Did Change %@",[(NSTextField*) notification.object stringValue] );
+    NSLog(@"Last Change %@", last );
+    
+    [self addExpression:last];
+    [self refreshScreen];
+    
+}
+
+- (BOOL)control:(NSControl *)control textView:(NSTextView *)textView doCommandBySelector:(SEL)commandSelector{
+    
+    NSLog(@"Command Selector %@ ",  NSStringFromSelector(commandSelector));
+    
+    return true;
+}
+
+//Detecting Key Down
+- (void)keyDown:(NSEvent *)event{
+    NSLog(@"Key Down %@",event);
 }
 
 
